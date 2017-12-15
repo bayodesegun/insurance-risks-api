fea3b48bfd1ccd0199f65ae76c3c72030756b573
Bayode Aderinola <bayodesegun@gmail.com>
2017-12-15 05:24:35 +0100

    Redirect / to admin site urls

--- risks/urls.py
+++ risks/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import RiskListView, RiskDataView
 from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib import admin
 
 urlpatterns = {
 	url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 	url(r'^risks/$', RiskListView.as_view(), name="risks"),
+	url(r'^', admin.site.urls), # redirect / to admin sites
     url(r'^risks/(?P<pk>[0-9]+)/$', RiskDataView.as_view(), name='risk_data'),
     url(r'^obtain-token/', obtain_auth_token),
 }
