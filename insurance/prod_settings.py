3aa2b1567f27f248b02f43055ec1ffe86ca72f9e
Bayode Aderinola <bayodesegun@gmail.com>
2017-12-27 22:33:00 +0100

    Add support for Zappa-managed static files

--- insurance/prod_settings.py
+++ insurance/prod_settings.py
@@ -30,3 +30,13 @@ DATABASES = {
     }
 
 }
+
+# STATIC_URL = '/static/'
+
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
--- insurance/settings.py
+++ insurance/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'risks',
     'rest_framework.authtoken',
+    'storages',
 ]
 
 MIDDLEWARE = [
