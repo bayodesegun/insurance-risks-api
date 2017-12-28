adf27f1bfd2d6e78a22f801d81df5452b172eda4
Bayode Aderinola <bayodesegun@gmail.com>
2017-12-28 06:24:43 +0100

    rephrase sentences

--- risks/authorizations.py
+++ risks/authorizations.py
@@ -3,7 +3,7 @@ from .models import Risk
 
 
 class IsRiskOwner(BasePermission):
-    """Custom permission class to allow bucketlist owners to edit them."""
+    """Custom permission class to only allow risk owners to edit them."""
 
     def has_object_permission(self, request, view, obj):
         """Return True if permission is granted to to access Risk object."""
--- risks/views.py
+++ risks/views.py
@@ -7,7 +7,7 @@ from .authorizations import IsRiskOwner
 from .models import Risk
 
 class RiskListView(generics.ListAPIView):
-    """This class handles the http requests for a list of risks owned by the user (insurer)."""
+    """This class handles http requests for a list of risks owned by the user (insurer)."""
     serializer_class = RiskListSerializer
     permission_classes = (
         permissions.IsAuthenticated,
@@ -17,7 +17,7 @@ class RiskListView(generics.ListAPIView):
         return Risk.objects.filter(insurer=self.request.user)
 
 class RiskDataView(generics.RetrieveAPIView):
-    """This class handles the http requests for risk data for a specified risks"""
+    """This class handles http requests for risk data (details) for a specified risk"""
     queryset = Risk.objects.all()
     serializer_class = RiskDataSerializer
     permission_classes = (
